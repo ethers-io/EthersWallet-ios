e5a9f057b134a8bd21f04a51b0a801a303b2dde1
ricmoo <me@ricmoo.com>
2017-02-02 14:57:09 -0500

    Simplified font build scripts.

--- EthersWallet/Assets/Build/fonts-ethers/build.sh
+++ EthersWallet/Assets/Build/fonts-ethers/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#/usr/bin/git checkout -- builder/*.json
-
-#/usr/bin/python ./builder/generate.py
-#/bin/cp css/* ../website/css/
-#/bin/cp fonts/* ../website/fonts/
-
-rm -rf fonts/*.ttf
+rm -rf fonts/ethers.ttf
 fontforge -script $PWD/builder/scripts/generate_font.py
 cp ./fonts/ethers.ttf ../../Fonts/ethers.ttf
--- EthersWallet/Assets/Build/fonts-ethers/builder/scripts/generate_font.py
+++ EthersWallet/Assets/Build/fonts-ethers/builder/scripts/generate_font.py
@@ -4,16 +4,12 @@
 
 import fontforge
 import os
-import md5
 import subprocess
 import tempfile
-import json
-import copy
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 INPUT_SVG_DIR = os.path.join(SCRIPT_PATH, '..', '..', 'src')
 OUTPUT_FONT_DIR = os.path.join(SCRIPT_PATH, '..', '..', 'fonts')
-MANIFEST_PATH = os.path.join(SCRIPT_PATH, '..', 'manifest.json')
 AUTO_WIDTH = True
 KERNING = 15
 
@@ -26,8 +22,6 @@ f.descent = 64
 
 font_name = 'ethers'
 
-manifest_data = dict(name = font_name, icons = [])
-
 for dirname, dirnames, filenames in os.walk(INPUT_SVG_DIR):
   for filename in filenames:
     name, ext = os.path.splitext(filename)
@@ -38,11 +32,6 @@ for dirname, dirnames, filenames in os.walk(INPUT_SVG_DIR):
       chr_code = hex(ord(filename.split('-')[0]))
 
       print '%s - %s' % (filename, chr_code)
-
-      manifest_data['icons'].append({
-        'name': name,
-        'code': chr_code
-      })
 
       if ext in ['.svg']:
         # hack removal of <switch> </switch> tags
@@ -85,36 +74,6 @@ f.fontname = font_name
 f.familyname = font_name
 f.fullname = font_name
 f.generate(fontfile + '.ttf')
-f.generate(fontfile + '.svg')
-
-# Fix SVG header for webkit
-# from: https://github.com/fontello/font-builder/blob/master/bin/fontconvert.py
-svgfile = open(fontfile + '.svg', 'r+')
-svgtext = svgfile.read()
-svgfile.seek(0)
-svgfile.write(svgtext.replace('''<svg>''', '''<svg xmlns="http://www.w3.org/2000/svg">'''))
-svgfile.close()
-
-scriptPath = os.path.dirname(os.path.realpath(__file__))
-try:
-  subprocess.Popen([scriptPath + '/sfnt2woff', fontfile + '.ttf'], stdout=subprocess.PIPE)
-except OSError:
-  # If the local version of sfnt2woff fails (i.e., on Linux), try to use the
-  # global version. This allows us to avoid forcing OS X users to compile
-  # sfnt2woff from source, simplifying install.
-  subprocess.call(['sfnt2woff', fontfile + '.ttf'])
-
-# eotlitetool.py script to generate IE7-compatible .eot fonts
-subprocess.call('python ' + scriptPath + '/eotlitetool.py ' + fontfile + '.ttf -o ' + fontfile + '.eot', shell=True)
-subprocess.call('mv ' + fontfile + '.eotlite ' + fontfile + '.eot', shell=True)
 
 # Hint the TTF file
 subprocess.call('ttfautohint -s -f -n ' + fontfile + '.ttf ' + fontfile + '-hinted.ttf > /dev/null 2>&1 && mv ' + fontfile + '-hinted.ttf ' + fontfile + '.ttf', shell=True)
-
-manifest_data['icons'] = sorted(manifest_data['icons'], key=lambda k: k['name'])
-
-print "Save Manifest, Icons: %s" % ( len(manifest_data['icons']) )
-f = open(MANIFEST_PATH, 'w')
-f.write( json.dumps(manifest_data, indent=2, separators=(',', ': ')) )
-f.close()
-
