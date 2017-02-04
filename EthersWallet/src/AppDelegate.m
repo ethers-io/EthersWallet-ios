ce9b7914ebd5ba1a3b505e0004ccc3c72c5cb423
ricmoo <me@ricmoo.com>
2017-02-04 03:46:56 -0500

    Fixed excessive payment pop-up bug (when using biometrics, the app seems to didBecomeActive once the modal is dismissed).

--- EthersWallet/src/AppDelegate.m
+++ EthersWallet/src/AppDelegate.m
@@ -200,32 +200,14 @@
     return nil;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-
-    [self notifyExtensions];
-
+- (void)checkAndAlertPasteboard {
     Payment *payment = [self checkPasteboard];
     if (!payment || [payment.address isEqualToAddress:_wallet.activeAccount] || !_wallet.activeAccount) {
         return;
     }
-
+    
     NSLog(@"Found: %@", payment);
-
+    
     [ModalViewController dismissAllCompletionCallback:^() {
         UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Clipboard Payment"
                                                                                  message:@"An Ethereum address was found in the clipboard."
@@ -257,7 +239,28 @@
         
         [ModalViewController presentViewController:alertController animated:YES completion:nil];
     }];
-    
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [self checkAndAlertPasteboard];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+    [self notifyExtensions];
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
